f3e7ff26a0d4b72df1cba4bec9035933e0efbe5c
Paul Savignano <paul.savignano@gmail.com>
2016-01-24 17:15:00 -0800

    Completed page 67 of 149

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,29 @@
-new Mongo.Collection('players');
+PlayersList = new Mongo.Collection('players');
+
+
+if(Meteor.isClient){
+
+
+
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'count': function(){
+      return PlayersList.find().count();
+    }
+  });
+
+  Template.leaderboard.events({
+    "click .player": function(event, template){
+      var playerId = "session value test";
+      Session.set('selectedPlayer', 'session value test');
+      Session.get('selectedPlayer');
+    }
+  });
+
+}
+
+if(Meteor.isServer){
+  console.log("Hello Server");
+}
